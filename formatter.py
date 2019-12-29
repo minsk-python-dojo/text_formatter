7648e1b7acfd7b7b454ae6fcabefbb21f856ef81
hartred <artiomse@gmail.com>
2019-12-29 16:44:04 +0300

    moved formatter logic to the 'formatter' module

---
+++ formatter.py
@@ -0,0 +1,49 @@
+"""
+Module for formatter logic
+"""
+
+from enum import Enum, auto, unique
+from typing import List, Dict, Callable
+
+@unique
+class Style(Enum):
+    """
+    Class for styles logic
+    """
+    HASH_BORDER = auto()
+    PLAIN_TEXT = auto()
+    AT_SIGN = auto()
+
+    @staticmethod
+    def all_styles() -> list:
+        """
+        Function realizes style logic
+        """
+        existing_styles = [
+            Style.HASH_BORDER,
+            Style.PLAIN_TEXT,
+            Style.AT_SIGN,
+        ]
+        return existing_styles
+
+
+def show_style(chosen_style: Style, raw_input: str):
+    """
+    Format raw_input with chosen style
+    """
+    def show_plain(text: str):
+        print(text)
+
+    def show_hashborder(text: str):
+        print(f'###\n{text}\n###')
+
+    def show_at_sign(text: str):
+        print(f'@@@@ {text} @@@')
+
+    style_table: Dict[Style, Callable] = {
+        Style.PLAIN_TEXT: show_plain,
+        Style.HASH_BORDER: show_hashborder,
+        Style.AT_SIGN: show_at_sign,
+    }
+    styler: Callable = style_table.get(chosen_style, Style.PLAIN_TEXT)
+    styler(raw_input)
